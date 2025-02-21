8ded0d6fe38e0e5abf05e591cc40a46b5104ac9f
xZeroShu <hacchi1096@gmail.com>
2025-02-21 17:38:01 +0100

    feat: add GetSineWaveBuffer and GetSilenceWaveBuffer methoid in NoteHelper class

--- MusicMage.Backend/Helpers/NoteHelper.cs
+++ MusicMage.Backend/Helpers/NoteHelper.cs
@@ -2,6 +2,7 @@ namespace MusicMage.Backend.Helpers;
 
 public static class NoteHelper
 {
+    public const int SampleRate = 44100; // Standard audio sample rate
     private static readonly Dictionary<string, float> NoteXFrequency = new()
     {
         ["C"] = 16.35f,
@@ -22,5 +23,29 @@ public static class NoteHelper
     {
         var noteToReturn = NoteXFrequency.GetValueOrDefault(note, 0);
         return noteToReturn * MathF.Pow(2, octave);
+    }
+    
+    public static byte[] GetSineWaveBuffer(double frequency, int durationMs)
+    {
+        var noteSampleCount = (int)((SampleRate * durationMs) / 1000.0);
+        
+        var noteBuffer = new short[noteSampleCount];
+        
+        const double amplitude = 16383d; // short.MaxValue / 2
+        var angleIncrement = 2.0 * Math.PI * frequency / SampleRate;
+
+        for (var i = 0; i < noteSampleCount; i++)
+        {
+            noteBuffer[i] = (short)(amplitude * Math.Sin(i * angleIncrement));
+        }
+
+        return noteBuffer.SelectMany(BitConverter.GetBytes).ToArray();
+    }
+
+    public static byte[] GetSilenceWaveBuffer(int pauseMs)
+    {
+        var pauseSampleCount = (int)((SampleRate * pauseMs) / 1000.0);
+        var silenceBuffer = new short[pauseSampleCount];
+        return silenceBuffer.SelectMany(BitConverter.GetBytes).ToArray();
     }
 }
